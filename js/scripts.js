db39232fdaa6aef1da2db1114cbd1a7569c5ae46
Malgorzata Haniszewska and Shane Robinson <malgorzata@>
2017-10-17 14:18:41 -0700

    add button class to change colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
-  $(".button").click(function() {
+  $(".btn-spain").click(function() {
     $(".show").slideToggle();
     //$(".show").slideUp();
 
   });
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+  });
+
 });
